b8c6e7079a421c91d520436f7aaf2dbd2ba4884b
Amirkhon <amirkhonishakulov@gmail.com>
2023-09-05 21:33:02 -0400

    Its Amirkhon 02

--- Andrew2.java
+++ Andrew2.java
@@ -1,5 +1,6 @@
 public class Andrew2 {
     public static void main(String[] args) {
-        System.out.println("This is my code");
+        System.out.println("This is my text");
+        System.out.println("Hello Andrew");
     }
 }
