226169f42c70c4db80ec90bb92d7151bbf4e958f
emelyy2 <mely.romero1596@gmail.com>
2023-09-06 21:14:19 -0400

    More practice

--- Andrew2.java
+++ Andrew2.java
@@ -1,6 +1,8 @@
 public class Andrew2 {
+
     public static void main(String[] args) {
         System.out.println("This is my text");
         System.out.println("Hello Andrew");
+        System.out.println("Test Emely code");
     }
 }
